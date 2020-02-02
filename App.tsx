51ee9671d338a1567855c670d636414a4b59a8c6
MatHuf <53545249+MatHuf@users.noreply.github.com>
2020-02-01 20:26:53 -0500

    Added apollo

--- App.tsx
+++ App.tsx
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 import Home from "./src/views/Home";
 import Details from "./src/views/Detail";
 
+// https://www.apollographql.com/docs/react/integrations/react-native/
+import { AppRegistry } from "react-native";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
 // https://reactnavigation.org/docs/en/hello-react-navigation.html
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://spacexdata.herokuapp.com/graphql"
+  }),
+  cache: new InMemoryCache()
+});
 
 const AppNavigator = createStackNavigator(
   {
@@ -22,6 +36,12 @@ const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <ApolloProvider client={apolloClient}>
+        <AppContainer />
+      </ApolloProvider>
+    );
   }
 }
+
+AppRegistry.registerComponent("SpaceXpo", () => App);
--- src/views/Detail.tsx
+++ src/views/Detail.tsx
@@ -1,6 +1,45 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+interface LandingPad {
+  id: string;
+  full_name: number;
+}
+
+interface LandingPadData {
+  landingpads: LandingPad[];
+}
+
+interface LandingPadVars {
+  limit: number;
+}
+
+const GET_LANDING_PADS = gql`
+  {
+    landingpads {
+      full_name
+    }
+  }
+`;
+
+/* {
+    landingpads {
+      full_name
+      location {
+        name
+        region
+      }
+      landing_type
+      attempted_landings
+      successful_landings
+      status
+      details
+    }
+  } */
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,14 +52,27 @@ const styles = StyleSheet.create({
   }
 });
 
-class Detail extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Details here...</Text>
-      </View>
-    );
-  }
-}
+const Detail = props => {
+  const { loading, data } = useQuery<LandingPadData, LandingPadVars>(
+    GET_LANDING_PADS,
+    { variables: { limit: 10 } }
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text>Landing Pads</Text>
+      {loading ? (
+        <Text style={styles.title}>Loading ...</Text>
+      ) : (
+        data &&
+        data.landingpads.map((pad, i) => (
+          <Text key={pad.id} style={styles.title}>
+            {pad.full_name}
+          </Text>
+        ))
+      )}
+    </View>
+  );
+};
 
 export default Detail;
