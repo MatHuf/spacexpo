fe6b4ded4a3be7d96b5b58d49032e090f85eff85
MatHuf <53545249+MatHuf@users.noreply.github.com>
2020-02-09 09:07:37 -0500

    Deconstruting prop

--- components/listItems/HistoryListItem.js
+++ components/listItems/HistoryListItem.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Text, View } from "react-native";
 import { listViewStyles as styles } from "../../styles";
 
-export default function HistoryListItem(props) {
+export default function HistoryListItem({ item }) {
   return (
     <View style={styles.item}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>{props.item.title}</Text>
+        <Text style={styles.headerText}>{item.title}</Text>
 
-        <Text style={styles.subHeaderText}>{props.item.event_date_utc}</Text>
+        <Text style={styles.subHeaderText}>{item.event_date_utc}</Text>
       </View>
       <View style={styles.detailContainer}>
-        <Text style={styles.detail}>{props.item.details}</Text>
+        <Text style={styles.detail}>{item.details}</Text>
       </View>
     </View>
   );
